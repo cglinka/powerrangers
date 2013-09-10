7635d02b2b5807b80d74281ab9797d053ebe1589
Clare Glinka <glinka.cb@gmail.com>
2013-09-10 18:02:36 -0500

    Add Person class with methods run, scream, and drink_coffee

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,24 @@
 class Person
+  @name
+  @caffeine_level
+
+  def initialize(name)
+    @name = name
+    @caffeine_level=0
+  end
+
+  def run
+    return "#{@name} runs!"
+  end
+
+  def scream(input)
+    return "#{@name} screams #{input.upcase()}!"
+  end
+
+  def drink_coffee(number)
+    @caffeine_level += number
+    return "#{@name} drinks #{number} cup(s) of coffee."
+  end
 end
 
 class PowerRanger
@@ -6,3 +26,8 @@ end
 
 class EvilNinja
 end
+
+bob = Person.new('Bob')
+puts bob.scream("help")
+puts bob.run
+puts bob.drink_coffee(3)
