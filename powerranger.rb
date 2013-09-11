a450bd43b49b978a23cd4898d4a2e74db68dfec0
Clare Glinka <glinka.cb@gmail.com>
2013-09-10 20:01:01 -0500

    Add PowerRanger class with punch and rest methods. Not included: use_megazord.

--- powerranger.rb
+++ powerranger.rb
@@ -1,18 +1,17 @@
 class Person
-  @name
-  @caffeine_level
+  attr_accessor :name, :caffeine_level
 
   def initialize(name)
     @name = name
-    @caffeine_level=0
+    @caffeine_level=100
   end
 
   def run
     return "#{@name} runs!"
   end
 
-  def scream(input)
-    return "#{@name} screams #{input.upcase()}!"
+  def scream
+    return "#{@name} screams AHHHHHHHHH!"
   end
 
   def drink_coffee(number)
@@ -21,13 +20,43 @@ class Person
   end
 end
 
-class PowerRanger
+class PowerRanger < Person
+
+  # def self.use_megazord(person)
+    
+  # end
+
+  def initialize(name, color, strength)
+    super(name)
+    @color = color
+    @strength = strength
+  end
+
+  def punch(person)
+    person.scream
+    if @strength > 5
+      person.run
+      person.caffeine_level -= @strength
+    end
+    @caffeine_level -= (@strength/2)
+    return "#{@name} punched #{person.name}!"
+  end
+
+  def rest
+    @strength = @strength + 1
+  end
 end
 
-class EvilNinja
+class EvilNinja < Person
 end
 
+#-----------TESTING Person CLASS-----------#
 bob = Person.new('Bob')
-puts bob.scream("help")
+puts bob.scream
 puts bob.run
 puts bob.drink_coffee(3)
+
+#-----------TESTING PowerRanger CLASS-----------#
+jane = PowerRanger.new("Jane", "Yellow", 10)
+puts jane.scream
+puts jane.punch(bob)
